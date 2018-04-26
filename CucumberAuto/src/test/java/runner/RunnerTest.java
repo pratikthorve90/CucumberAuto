b8eba04d038b7a12cebba4a89707dfe29c624811
Pratik Thorve <pratikthorve90@gmail.com>
2018-04-27 00:27:25 +0530

    After commneting most of the runner classes as it slow my machines during exection

--- CucumberAuto/src/test/java/runner/RunnerTest.java
+++ CucumberAuto/src/test/java/runner/RunnerTest.java
@@ -5,25 +5,25 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		
-		monochrome=true,
-		
-		features= {"src/test/resources/FeatureFiles/"},
-		
-		dryRun=false,
-		
-		glue= {"stepDef"},
-		
-		plugin= {"pretty",
-				 "html:target/cucumber-htmlreprot",
-				 "json:target/cucumber-report-RunnerTest.json",
-				 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport_RunnerTest.html"
-		}
-		
-		)
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//		
+//		monochrome=true,
+//		
+//		features= {"src/test/resources/FeatureFiles/"},
+//		
+//		dryRun=false,
+//		
+//		glue= {"stepDef"},
+//		
+//		plugin= {"pretty",
+//				 "html:target/cucumber-htmlreprot",
+//				 "json:target/cucumber-report-RunnerTest.json",
+//				 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport_RunnerTest.html"
+//		}
+//		
+//		)
 
 public class RunnerTest {
 
--- CucumberAuto/src/test/java/runner/hooksTest.java
+++ CucumberAuto/src/test/java/runner/hooksTest.java
@@ -5,25 +5,25 @@ import org.junit.runner.RunWith;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-		
-		monochrome=true,
-		
-		features= {"src/test/resources/FeatureFiles/hooks.feature"},
-		
-		dryRun=false,
-		
-		glue= {"stepDef"},
-		
-		plugin= {"pretty",
-				 "html:target/cucumber-htmlreprot",
-				 "json:target/cucumber-report-hooksTest.json",
-				 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport_hooksTest.html"
-		}
-		
-		)
+//
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//		
+//		monochrome=true,
+//		
+//		features= {"src/test/resources/FeatureFiles/hooks.feature"},
+//		
+//		dryRun=false,
+//		
+//		glue= {"stepDef"},
+//		
+//		plugin= {"pretty",
+//				 "html:target/cucumber-htmlreprot",
+//				 "json:target/cucumber-report-hooksTest.json",
+//				 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport_hooksTest.html"
+//		}
+//		
+//		)
 
 public class hooksTest {
 
